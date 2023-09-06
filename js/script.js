2094e727024888d42bf83dee20113ce66b177b29
k.sato <bjtfm000@gmail.com>
2023-09-07 07:30:32 +0900

    20230907
    new file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+    /*
+    $('#slick-top').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+        dotsClass: 'slick-dots2',
+    });
+    */
+    $('#header').load('header.html');
+    $('#footer').load('footer.html');
+});
